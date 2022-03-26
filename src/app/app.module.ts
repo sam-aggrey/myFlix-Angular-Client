27376a6b892bc7569afaf0ad9d8501ff5f34a061
sam aggrey <samaggrey@hotmail.com>
2022-03-26 15:44:18 +0000

    Implementing login and registration logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+//import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+UserRegistrationFormComponent,
+UserLoginFormComponent,
+//UserLoginFormComponent,
+],
+imports: [
+BrowserModule,
+AppRoutingModule,
+BrowserAnimationsModule,
+MatCardModule,
+MatFormFieldModule,
+MatDialogModule,
+FormsModule,
+HttpClientModule,
+MatSnackBarModule,
+MatInputModule,
+MatButtonModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,55 @@
+/**
+ * The UserLoginFormComponent is used to render a mat dialog containing a form where the
+ * user can submit their credentials to log in to myFlix.
+ * @module UserLoginFormComponent
+ */
+
+ import { Component, OnInit } from '@angular/core';
+ import { MatDialogRef } from '@angular/material/dialog';
+ // Used to access the loginUser function created on this service
+ import { FetchApiDataService } from '../fetch-api-data.service';
+ import { MatSnackBar } from '@angular/material/snack-bar';
+ // Used to navigate the user to the movies route on a successful login
+ import { Router } from '@angular/router';
+
+ @Component({
+   selector: 'app-user-login-form',
+   templateUrl: './user-login-form.component.html',
+   styleUrls: ['./user-login-form.component.scss']
+ })
+
+ export class UserLoginFormComponent implements OnInit {
+
+   loginData = { Username: '', Password: '' };
+
+   constructor(
+     public fetchApiData: FetchApiDataService,
+     public dialogRef: MatDialogRef<UserLoginFormComponent>,
+     public snackBar: MatSnackBar,
+     public router: Router
+   ) { }
+
+   ngOnInit(): void {
+    }
+
+
+   userLogin(): void {
+     this.fetchApiData.userLogin(this.loginData).subscribe((response) => {
+       this.dialogRef.close();
+
+
+      localStorage.setItem('password', this.loginData.Password);
+      localStorage.setItem('user', response.user.Username);
+      localStorage.setItem('token', response.token);
+      console.log(response);
+      this.snackBar.open(`Hi ${this.loginData.Username}. You're logged in to myFlix!`, 'Cool!', { duration: 4000, panelClass: 'snack-style' });
+      this.router.navigate(['movies']); // Navigates to the movies route
+     }, (response) => {
+       console.log(response);
+       this.snackBar.open(`Sorry ${this.loginData.Username} we couldn't log you in. Please check your username and password`, 'Ok',
+         { duration: 4000, panelClass: 'snack-style' }
+       );
+     });
+   }
+
+ }
